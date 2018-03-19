dd1e4549979de0ed53c04c460fe909bf7a133d59
Stefan Oehrli <stefan.oehrli@trivadis.com>
2018-03-19 09:14:08 +0100

    Finish OracleJava build context

--- OracleJava/README.md
+++ OracleJava/README.md
@@ -14,12 +14,23 @@ $ cd java-8
 $ docker build -t oracle/serverjre:8 .
 ```
 
-Alternatively you may use the shell script `build.sh`:
+Alternatively you may use the shell script `build.sh` to build the docker images. The script does check if a server JRE package (`server-jre-8u<UPDATE>-linux-x64.tar.gz`) is available. Otherwise it either tries to fallback to a local Java patch package (eg. `p<PATCHID>_180<UPDATE>_Linux-x86-64.zip`) or download the Java patch package from MOS using [curl](https://linux.die.net/man/1/curl). Since you have to be logged in to MOS, curl needs a `.netrc` file with the MOS credentials.
+
+Build with a local `server-jre-8u<UPDATE>-linux-x64.tar.gz` or `p<PATCHID>_180<UPDATE>_Linux-x86-64.zip` file:
 
 ```
 $ cd java-8
 $ ./build.sh
 ```
+
+Create a `.netrc` file containing the credentials for *login.oracle.com* to automatically download the Java patch package before building the docker image:
+
+```
+    $ cd java-8
+    $ echo "machine login.oracle.com login <MOS_USER> password <MOS_PASSWORD>" >.netrc
+    $ ./build.sh
+```
+
 ## License
 
 To download and run Oracle Java, regardless whether inside or outside a Docker container, you must download the binaries from the Oracle website and accept the license indicated at that page.
--- OracleJava/java-8/build.sh
+++ OracleJava/java-8/build.sh
@@ -9,7 +9,10 @@
 # Date.......: 2018.03.13
 # Revision...:  
 # Purpose....: Build script to build Oracle Java 1.8
-# Notes......: --
+# Notes......: The script does look for the server JRE package. If not 
+#              available it does fall back to the java patch package or 
+#              tries to download the java patch package from MOS. The
+#              download does require a .netrc file with the MOS credentials
 # Reference..: --
 # License....: Licensed under the Universal Permissive License v 1.0 as 
 #              shown at http://oss.oracle.com/licenses/upl.
@@ -17,10 +20,23 @@
 # Modified...:
 # see git revision history for more information on changes/updates
 # -----------------------------------------------------------------------------
+
+# - Customization -----------------------------------------------------------
+# copy past the latest java download url
+JAVA_URL="https://updates.oracle.com/Orion/Services/download/p27217289_180162_Linux-x86-64.zip?aru=21855272&patch_file=p27217289_180162_Linux-x86-64.zip"
 DOCKER_REPOSITORY="oracle"
 DOCKER_IMAGE_NAME="serverjre:8"
+# - End of Customization ----------------------------------------------------
+
+# - Default Values ----------------------------------------------------------
 DOCKERFILE="$(dirname $0)/Dockerfile"
 DOCKER_BUILD_DIR="$(cd $(dirname $0) 2>&1 >/dev/null; pwd -P)"
+# get java package name from download url
+JAVA_PKG=${JAVA_URL#*patch_file=}
+# get java update from java package name
+JAVA_UPDATE=$(echo $JAVA_PKG|sed 's/.*180\([0-9]\{3\}\)_.*/\1/')
+# define server jre package name 
+SERVER_JRE_PACKAGE="server-jre-8u${JAVA_UPDATE}-linux-x64.tar.gz"
 
 if [ ! -f "${DOCKERFILE}" ]; then
     echo " ERROR : Can not build ${DOCKER_IMAGE_NAME}. Please run build.sh "
@@ -32,5 +48,24 @@ echo " Build docker image ${DOCKER_REPOSITORY}/${DOCKER_IMAGE_NAME} using"
 echo " Dockerfile             : ${DOCKERFILE}"
 echo " Docker build directory : ${DOCKER_BUILD_DIR}" 
 echo "--------------------------------------------------------------------------------"
+
+# check if there is a server jre package
+if [ -f "${DOCKER_BUILD_DIR}/${SERVER_JRE_PACKAGE}" ]; then
+    echo "Did found a server JRE 1.8 package ${SERVER_JRE_PACKAGE}"
+elif [ -f "${DOCKER_BUILD_DIR}/${JAVA_PKG}" ]; then
+    echo "Did found server JRE 1.8 patch package ${JAVA_PKG}"
+    echo "Unzip ${JAVA_PKG}"
+    unzip -o ${DOCKER_BUILD_DIR}/${JAVA_PKG}
+elif [ -f "${DOCKER_BUILD_DIR}/.netrc" ]; then
+    echo "Try to download Server JRE 8u${JAVA_UPDATE} patch package from MOS"
+    curl --netrc-file ${DOCKER_BUILD_DIR}/.netrc --cookie-jar cookie-jar.txt \
+        --location-trusted ${JAVA_URL} -o ${JAVA_PKG} && unzip -o ${JAVA_PKG}
+    rm -f cookie-jar.txt
+else
+    echo "Missing Server JRE 8u${JAVA_UPDATE} package. "
+    echo "Fallback to ${JAVA_PKG} or MOS download failed"
+    echo "Can not build docker image ${DOCKER_REPOSITORY}/${DOCKER_IMAGE_NAME}"
+    exit 1
+fi
 
 docker build -t ${DOCKER_REPOSITORY}/${DOCKER_IMAGE_NAME} -f "${DOCKERFILE}" "${DOCKER_BUILD_DIR}"
