4ef24a05ee83c8229d2aa63257d3ee09b3ef27cf
Stefan Oehrli <stefan.oehrli@trivadis.com>
2018-12-10 22:11:30 +0100

    docker compose for database 18.4.0.0 sample

---
+++ samples/18.4.0.0/config/setup/01_create_scott.sql
@@ -0,0 +1,20 @@
+----------------------------------------------------------------------------
+--  Trivadis AG, Infrastructure Managed Services
+--  Saegereistrasse 29, 8152 Glattbrugg, Switzerland
+----------------------------------------------------------------------------
+--  Name......: 01_create_scott.sql
+--  Author....: Stefan Oehrli (oes) stefan.oehrli@trivadis.com
+--  Editor....: Stefan Oehrli
+--  Date......: 2018.12.10
+--  Revision..:  
+--  Purpose...: Script to create the SCOTT schema
+--  Notes.....:  
+--  Reference.: SYS (or grant manually to a DBA)
+--  License...: Licensed under the Universal Permissive License v 1.0 as 
+--              shown at http://oss.oracle.com/licenses/upl.
+----------------------------------------------------------------------------
+--  Modified..:
+--  see git revision history for more information on changes/updates
+----------------------------------------------------------------------------
+@?/rdbms/admin/utlsampl.sql
+-- EOF ---------------------------------------------------------------------
